bf256db7bc3c90350a8fc6275f6dc86d375dd9e5
oriol <obassols8@boscdelacoma.cat>
2022-09-15 18:20:58 +0200

    Variables Primitius

--- UF1/A1/VariablesPrimitius.php
+++ UF1/A1/VariablesPrimitius.php
@@ -1,7 +1,25 @@
 <?php
+// $i és una variable de tipus integer
 $i = 12;
-$tipus_de_i = gettype( $i );
-echo "La variable \$i 
-      conté el valor $i 
-	  i és del tipus $tipus_de_i";
+gettypevar( $i , "\$i");
+
+// $b és una variable de tipus boolea
+$b = true;
+gettypevar( $b , "\$b");
+
+// $f és una variable de tipus coma flotant (double)
+$f = 0.5;
+gettypevar( $f , "\$f");
+
+// $s és una variable de tipus cadena de caracters (string)
+$s = "Això és una cadena de caràcters";
+gettypevar( $s , "\$s");
+
+$tipus_de_gettype = gettype( gettype( $i ) );
+echo "El tipus del valor que retorna el gettype  és $tipus_de_gettype <br>\n";
+
+function gettypevar( $var, $varname ) {
+    $tipus_de_var = gettype( $var );
+    echo "La variable $varname conté el valor $var i és del tipus $tipus_de_var <br>\n";
+}
 ?>
