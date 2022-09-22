8fda41aa298cb63212d81f1b172b4cd30f461d33
oriol <obassols8@boscdelacoma.cat>
2022-09-22 18:45:47 +0200

    Factorial Array

---
+++ UF1/A1/ArraysFuncions/FactorialArray.php
@@ -0,0 +1,31 @@
+<?php
+$test_array = [4,5,2,3,6];
+
+var_dump($test_array);
+echo "<br>";
+var_dump(factorialArray($test_array));
+
+function factorialArray( array $array ): bool | array  {
+    $factorial_array = array();
+
+    foreach($array as $number) {
+        // Comprova que el $number sigui un numero o si es un numero negatiu
+        if (is_int($number) && $number > -1) {
+            array_push($factorial_array, factorialNombre($number));
+        } else {
+            return false;
+        }
+    }
+    return $factorial_array;
+}
+
+function factorialNombre( int $num ) {
+    // Si el numero és mes gran a 1 crida la mateixa funcio amb el $num - 1, sino retorna 1
+    if ( $num > 1 ) {
+        return $num * factorialNombre( $num - 1 );
+    } else {
+        return 1;
+    }
+}
+
+?>
